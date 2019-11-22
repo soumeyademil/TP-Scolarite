8b39560d08308de46e5adf42ea18c621fb27e385
Soumeya <hs_demil@esi.dz>
2019-11-22 18:21:31 +0100

    Creation modeles

---
+++ tests/SeleniumExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Modelizer\Selenium\SeleniumTestCase;
+
+class SeleniumExampleTest extends SeleniumTestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->visit('/');
+    }
+}
