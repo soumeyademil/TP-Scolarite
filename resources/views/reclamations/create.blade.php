a5b61a938a706dbe714b9eccd83b0728bfd485c1
Soumeya <hs_demil@esi.dz>
2019-11-25 19:14:30 +0100

    Creation et test de reclamations.create

--- resources/views/reclamations/create.blade.php
+++ resources/views/reclamations/create.blade.php
@@ -1,34 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<<<<<<< HEAD
-<h1>Vous pouvez ajouter une reclamation</h1>
-=======
-<!--<h1>Vous pouvez ajouter une reclamation</h1>
->>>>>>> br
-{!! Form::open(['action' => 'ReclamController', 'method' => 'POST']) !!}
-    <div class="form-group">
-        {{Form::label('body', 'Contenu')}}
-        {{Form::textarea('body','',['class' => 'form-control', 'placeholder' => 'Votre reclamation..'])}}
-        </div>
-{!! Form::close() !!}
-<<<<<<< HEAD
-
-=======
-<form action="{{ route('reclamation@store') }}" method="POST">
-<div class="form-group">
-        <input type="text" class="form-control" name="etud" placeholder="entrez votre matricule"><br>        
-        <input type="text" class="form-control" name="gr" placeholder="entrez le groupe"><br>
-        <input type="text" name="mod" class="form-control" placeholder="entrez le module"><br>
-</div>
-<div class="form-group">     
-        <textarea name="contenu" class="form-control" placeholder="votre reclamation" rows="10"></textarea><br>
-</div>
-<div>
-        <button type="submit" class="btn btn-succes">Envoyer</button>
-</div>
-
-</form>-->
 <div class="container">
         <div class="row">
                 <div class="col-md-12">
@@ -60,5 +32,4 @@
                 </div>
         </div>
 </div>
->>>>>>> br
 @endsection
