7a9fc2ef57e53e70588a094695b54d90b60a6c2d
Soumeya <hs_demil@esi.dz>
2019-11-26 20:08:11 +0100

    aucune modification impostante

--- resources/views/reclamations/create.blade.php
+++ resources/views/reclamations/create.blade.php
@@ -1,44 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-
-<!-----------En cas d'erreurs------------->
-@if(count($errors))
-<div class="alert alert-danger" role="alert">
- <ul class="">
-        @foreach($errors->all() as $message)
-        <li class="">{{ $message }}</li>
-        @endforeach
- </ul>
-</div>
-@endif
-
-
 <div class="container">
         <div class="row">
                 <div class="col-md-12">
                         <form action="{{ url('reclamations') }}" method="post">
 
-                                {{ csrf_field() }}
+                        {{ csrf_field() }}
 
-                                <div class="form-group">
-                                        <label for="">Etudiant</label>
+                                <!-- <div class="form-group">
+                                        <label for=""></label>
                                         <input type="text" name="etud" class="form-control">
-                                </div>
+                                </div> -->
 
                                 <div class="form-group">
-                                        <label for="">Groupe</label>
+                                        <label for=""></label>
                                         <input type="text" name="gr" class="form-control">
                                 </div>
 
                                 <div class="form-group">
-                                        <label for="">Module</label>
-                                        <input type="text" name="mod" class="form-control" value="{{ old('mod') }}">
+                                        <label for=""></label>
+                                        <input type="text" name="mod" class="form-control">
                                 </div>
 
                                 <div class="form-group">
-                                        <label for="">Contenu</label>
-                                        <textarea name="contenu" class="form-control">{{ old('contenu') }}</textarea>
+                                        <label for=""></label>
+                                        <input type="text" name="contenu" class="form-control">
                                 </div>
 
                                 <div class="form-group">
