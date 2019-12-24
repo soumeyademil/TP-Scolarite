f7b6d4906954b148034062716dcaf955b72ddad0
Soumeya DEMIL <hs_demil@esi.dz>
2019-12-24 13:46:29 +0100

    modification reclamation ..

--- resources/views/reclamations/create.blade.php
+++ resources/views/reclamations/create.blade.php
@@ -25,7 +25,7 @@
 
                                 <div class="form-group">
                                         <label for=""></label>
-                                        <input type="text" name="contenu" class="form-control">
+                                        <textarea cols="50" rows="2" name="contenu" class="form-control"></textarea>
                                 </div>
 
                                 <div class="form-group">
