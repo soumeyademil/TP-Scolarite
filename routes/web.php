ff74ec1093d85425096ca25ff5593395c9a97666
Soumeya DEMIL <hs_demil@esi.dz>
2020-01-03 16:20:33 +0100

    docker en cours pour reclamation: quelques problemes de bdd

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
